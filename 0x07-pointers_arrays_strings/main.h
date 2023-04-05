9a6e7ef90f90a1410c2274e090c83d8479950cc4
luckieyy <luckiey2004@gmail.com>
2023-04-05 05:05:30 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#dndef MAIN_H
+#define MAIN_H
+
+char * _memset(char 's, char b, unsigned int n);
+in anemateonsen
+char "_memcpy(char "dest, char 'sro, unsigned int n);
+strehr(char *s, char c). unsioned int sirson char"s, chat acceps)
+char strobrichar 's, char "acceoty 
+void print_chessboard(char (*aX8]). 
+void print diagsums (int 'a, int size)
+vol sel string char's, chat to),
